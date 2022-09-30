e80efd092e0a5604dfc5765bda25080c81b15a89
lucianoreguera <regueraluciano@gmail.com>
2022-09-30 13:36:05 -0300

    lifecycle components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnChanges, OnDestroy, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnChanges, OnDestroy {
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log('Change ->', changes);
+  }
+  ngOnInit(): void {
+    console.log('OnInit ->');
+  }
+  ngOnDestroy(): void {
+    console.log('Destroy ->');
+  }
   cities = ['Catamarca', 'Córdoba', 'San Luis'];
   name!: string;
   title = 'Aprendiendo Angular';
